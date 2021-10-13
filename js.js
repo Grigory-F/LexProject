2e817aca6bcef53504b031e081b873d369edaf16
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2021-10-13 21:56:06 +0300

    adwwaddaw

--- js.js
+++ js.js
@@ -1,12 +1,92 @@
-document.querySelector(".burger-button").addEventListener("click", () => {
-    document.querySelector(".main-nav__list-link").classList.toggle("main-nav__list-link--visible");
+/* document.querySelector(".burger-button").addEventListener("click", () => {
+  document.querySelector(".main-nav__list-link").classList.toggle("main-nav__list-link--visible");
 })
 document.querySelector(".copy-button").addEventListener("click", () => {
-    navigator.clipboard.writeText(document.querySelector('input').value)
-  .then(() => {
-    alert("Скопировано")
-  })
-  .catch(err => {
-    console.log('Something went wrong', err);
-  });
+  navigator.clipboard.writeText(document.querySelector('input').value)
+    .then(() => {
+      alert("Скопировано")
+    })
+    .catch(err => {
+      console.log('Something went wrong', err);
+    });
 })
+ */
+
+
+/* const Chart = require('node-modules/chart.js') */
+let ctx = document.querySelectorAll(".chart-canvas");
+for (let index = 0; index < ctx.length; index++) {
+  new Chart(ctx[index], {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+  
+}
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
