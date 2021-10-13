4dec39fe724bcfd24b560d436966eb4b4ded9280
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2021-10-13 16:12:16 +0300

    referal

--- js.js
+++ js.js
@@ -1,3 +1,12 @@
 document.querySelector(".burger-button").addEventListener("click", () => {
     document.querySelector(".main-nav__list-link").classList.toggle("main-nav__list-link--visible");
 })
+document.querySelector(".copy-button").addEventListener("click", () => {
+    navigator.clipboard.writeText(document.querySelector('input').value)
+  .then(() => {
+    alert("Скопировано")
+  })
+  .catch(err => {
+    console.log('Something went wrong', err);
+  });
+})
