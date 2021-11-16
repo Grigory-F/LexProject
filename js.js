96e5761c4d11295c193904cb2a253e35fb782521
grishka2500@gmail.com <57104420+Grigory-F@users.noreply.github.com>
2021-11-16 19:57:22 +0300

    example

--- js.js
+++ js.js
@@ -1,6 +1,14 @@
 document.querySelector(".burger-button").addEventListener("click", () => {
   document.querySelector(".main-nav__list-link").classList.toggle("main-nav__list-link--visible");
 })
+
+document.querySelector(".bell-button").addEventListener("click", (event) => {
+  event.target.classList.toggle("bell-button--active")
+  document.querySelector(".bell-button__header-popup").classList.toggle("bell-button__header-popup--active")
+});
+
+
+
 document.querySelector(".copy-button").addEventListener("click", () => {
   navigator.clipboard.writeText(document.querySelector('input').value)
     .then(() => {
